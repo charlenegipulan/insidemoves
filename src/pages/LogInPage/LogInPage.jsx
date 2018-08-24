511ea7276b4bb2261edda1cf9c7b8fcdf1025fb7
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-24 14:28:54 -0700

    render login page[

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Header from '../../components/Header/Header'
 import NavBar from '../../components/NavBar/NavBar'
+import LoginForm from '../../components/LogInForm/LogInForm';
 
 const LogInPage = (props) => {
-    
     return (
         <div className="LogInPage">
             <header className="LogInPage-header">
                 <Header />
                 <NavBar />
             </header>
+            <div>
+                <LoginForm />
+            </div>
         </div>
     )
 }
