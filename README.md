0eed7edb2efbece74734778b747964f092ad6bf2
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-09-02 14:04:05 -0700

    add order confirmation modal

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ An e-commerce storefront site for rare, collectible sneakers and jerseys. Built 
 
 ![Shoppage](https://i.imgur.com/1xKmT7o.png "Screenshot2")
 
+![Cart](https://i.imgur.com/3YsZVlm.png "Screenshot3")
+
 
 ## Technologies used
 * MongoDB / Mongoose
@@ -21,9 +23,10 @@ An e-commerce storefront site for rare, collectible sneakers and jerseys. Built 
 * Bootstrap
 
 ## Getting Started
-[Try a live demo here](https://insidemoves.herokuapp.com/)
+To try out a live demo, click [here](https://insidemoves.herokuapp.com/)
 
-[Check out my trello board for my planning here](https://trello.com/b/LmVXCwTB/project-4-inside-moves)
+To check out the planning phase in my Trello board, click [here](https://trello.com/b/LmVXCwTB/project-4-inside-moves)
+
 
 ## Unsolved Problems
 
--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -15,47 +15,69 @@ const PaymentPage = (props) => {
 
                     <form>
         {/* <div class="form-row"> */}
-            <div class="form-group col-lg-4">
+            <div>
             <label for="inputEmail4">First Name</label>
-            <input type="email" class="form-control" id="inputEmail4" placeholder="First Name" />
+            <input type="email" class="form-control" id="inputEmail4" placeholder="Charlene" />
             </div>
-            <div class="form-group col-lg-4">
+            <div>
             <label for="inputPassword4">Last Name</label>
-            <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+            <input type="password" class="form-control" id="inputPassword4" placeholder="Gipulan" />
             </div>
-        <div class="form-group col-lg-8">
+        <div>
             <label for="inputAddress">Address</label>
             <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
         </div>
-        <div class="form-group col-lg-8">
+        <div>
             <label for="inputAddress2">Address 2</label>
             <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
         </div>
-            <div class="form-group col-lg-6">
-            <label for="inputCity">City</label>
-            <input type="text" class="form-control" id="inputCity" />
+        <div class="row align-items-center justify-content-center">
+            <div>
+                <label for="inputCity">City</label>
+                <input type="text" class="form-control" id="inputCity" placeholder="Los Angeles"/>
             </div>
-        <div class="form-row">
-            <div class="form-group col-lg-2">
-            <label for="inputState">State</label>
-            <select id="inputState" class="form-control">
-                <option selected>CA</option>
-                <option>CA</option>
-                <option>AZ</option>
-                <option>KS</option>
-                <option>NY</option>
-                <option>NV</option>
-            </select>
+            &nbsp;&nbsp;&nbsp;
+            <div>
+                <label for="inputState">State</label> 
+                <select id="inputState" class="form-control">
+                    <option selected>CA</option>
+                    <option>CA</option>
+                    <option>AZ</option>
+                    <option>KS</option>
+                    <option>NY</option>
+                    <option>NV</option>
+                </select>
             </div>
-            <div class="form-group col-md-2">
-            <label for="inputZip">Zip</label>
-            <input type="text" class="form-control" id="inputZip"/> 
+            &nbsp;&nbsp;&nbsp;
+            <div>
+                <label for="inputZip">Zip</label>
+                <input type="text" class="form-control" id="inputZip" placeholder="91776"/> 
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <br />
+        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">Submit Order</button>
         </form>
             </div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                        <h3>Thank  you for your purchase!</h3>
+                        <p> You will recieve a confirmation email shortly.</p>
+                        <p> Your order number is 5379DF053C</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+        
     );
 }
 
